3b98c1a353bda71d95b1a90bbddc73b4ee566b0e
Eden Sulfurea <edensulfurea@users.noreply.github.com>
2026-03-25 17:08:20 -0400

    added localStorage diary and date

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import happyBg from './assets/photo/happy.jpg';
 import angryBg from './assets/photo/angry.jpg';
 import sadBg from './assets/photo/sad.jpg';
@@ -8,6 +8,21 @@ import depressedBg from './assets/photo/depressed.jpg';
 function App() {
   const [mood , setMood]= useState(""); 
   const [text, setText] = useState(""); 
+  const [date, setDate] = useState(""); 
+  const [entries, setEntries] = useState([]);
+  const saveEntry = () => {
+  const newEntry = {
+    mood,
+    text,
+    date
+  };
+
+  const updated = [...entries, newEntry];
+
+  setEntries(updated);
+
+  localStorage.setItem("moodEntries", JSON.stringify(updated));
+};
   const today = new Date().toLocaleDateString();
   const moods= {
     happy: happyBg,
@@ -16,16 +31,23 @@ function App() {
     excited: excitedBg,
     depressed: depressedBg
   };
+  useEffect(() => {
+  const saved = localStorage.getItem("moodEntries");
+
+  if (saved) {
+    setEntries(JSON.parse(saved));
+  }
+}, []);
  
   return (
     
       <div
-      style= {{
-        height: "100vh",
-        backgroundImage: mood? `url(${moods[mood]})`: "linear-gradient(to right, #667eea, #764ba2)",
-        backgroundSize: "cover",
-        backgroundPosition: "center"
-      }}>
+      style={{
+  height: "100vh",
+  background: mood
+    ? `url(${moods[mood]}) center/cover no-repeat`
+    : "linear-gradient(to right, #667eea, #764ba2)"
+}}>
         <h1>Choose your Mood...</h1>
         <h2>How are you feeling today? {mood}</h2>
         <h3>{today}</h3>
@@ -46,7 +68,23 @@ function App() {
       
 
       <p> {text}</p>
+      <input
+  type="date"
+  value={date}
+  onChange={(e) => setDate(e.target.value)}
+/>
+<button onClick={saveEntry}>
+  Save mood
+</button>
+<h3>Diary</h3>
 
+{entries.map((e, i) => (
+  <div key={i}>
+    <p>{e.date}</p>
+    <p>{e.mood}</p>
+    <p>{e.text}</p>
+  </div>
+))}
     
       </div>
       </div>
